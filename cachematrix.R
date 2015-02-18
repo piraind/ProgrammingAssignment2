462294f66fecceb63e06397eb99dfcb93b205b44
piraind <piraind@Davids-MacBook-Air-4.local>
2015-02-18 09:02:10 -0500

    Version 4 of assignment 2
    Matrix inversion with test commands

--- cachematrix.R
+++ cachematrix.R
@@ -5,12 +5,45 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m<-NULL
+      set<-function(y){
+            x<<-y
+            m<<-NULL
+      }
+      get<-function() x
+      setmatrix<-function(solve) m<<- solve
+      getmatrix<-function() m
+      list(set=set, get=get,
+           setmatrix=setmatrix,
+           getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+      m<-x$getmatrix()
+      if(!is.null(m)){
+            message("getting cached data")
+            return(m)
+      }
+      matrix<-x$get ()
+      m<-solve(matrix, ...)
+      x$setmatrix(m)
+      m
 }
+
+# test commands to see how functions work
+# Copy commands to R console
+      # create test list matrix
+      tmatrix <- makeCacheMatrix()
+
+      # set matrix in test list matrix
+      tmatrix$set(matrix(1:4,2,2))
+      str(tmatrix$get ()) # orginal matrix before first call
+      str(tmatrix$getmatrix ()) # cache before first call
+
+      # invert matirx first time
+      str(tmatrix$getmatrix ()) # cache before first call
+      cacheSolve(tmatrix)
+      str(tmatrix$getmatrix ()) # cache after first call
+
+      #invert matrix second time with cache
+      cacheSolve(tmatrix) 
